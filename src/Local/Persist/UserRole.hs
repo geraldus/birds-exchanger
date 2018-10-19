e32910bcbe28451af67988d0ca7a024ac37884fc
Артур Файзрахманов <heraldhoi@gmail.com>
2018-10-19 16:31:08 +0500

    Describe User Role data type

---
+++ src/Local/Persist/UserRole.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Local.Persist.UserRole where
+
+
+import Database.Persist.TH
+import Prelude
+
+
+data UserRole = Admin | Operator | Client
+    deriving (Show, Read, Eq)
+derivePersistField "UserRole"
