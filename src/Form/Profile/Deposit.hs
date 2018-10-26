8ede674fe6b5cb07db943d3f94c9061963a8b5ee
Артур Файзрахманов <heraldhoi@gmail.com>
2018-10-26 05:26:29 +0500

    Describe first restricted deposit form

---
+++ src/Form/Profile/Deposit.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Form.Profile.Deposit where
+
+
+import Import
+
+
+depositForm :: Form (FormResult DepositRequestFD, Widget)
+depositForm extra = do
+    (paymentCurrencyRes, paymentCurrencyView) <- mreq currencySelect "" Nothing
+    (paymentCurrencyAmountRes, paymentCurrencyAmountView) <- mreq doubleField "" Nothing
+    (paymentMethodRes, paymentMethodView) <- mreq (selectFieldList paymentMethodOptions) "" Nothing
+    (targetCurrencyRes, targetCurrencyView) <- mreq (selectFieldList currencyOptions) "" Nothing
+    let widget = do
+            inCurrencyId <- newIdent
+            inTargetCurrencyId <- newIdent
+            [whamlet|
+                #{extra}
+                <div ##{inCurrencyId}>
+                    ^{fvInput paymentCurrencyView}
+                <div ##{inTargetCurrencyId}>
+                    ^{fvInput targetCurrencyView}
+                <div>
+                    ^{fvInput paymentCurrencyAmountView}
+                ^{fvInput paymentMethodView}
+                |]
+    return (FormMissing, widget)
+
+
+data DepositRequestFD = DepositRequestFD
+    { depReqCurrency :: Currency
+    , depReqPaymentMethod :: PaymentMethod
+    , depReqCentsAmount :: Int
+    -- , depReqCentsExpectedFee :: Int
+    -- , depReqTransactionCode :: Text
+    , depReqTargetCurrency :: Currency
+    -- , depReqExpectedConversionRatio :: Double
+    -- , depReqCentsExpectedIncome :: Amount
+    }
+
+
+currencySelect = selectFieldList currencyOptions
+
+currencyOptions :: [(Text, Currency)]
+currencyOptions =
+    [ ("₽ российский рубль", FiatC RUR)
+    , ("PZM криптовалюта Prizm", CryptoC PZM) ]
+
+paymentMethodOptions :: [(Text, PaymentMethod)]
+paymentMethodOptions =
+    [ ("Перевод с карты на карту, ₽ российский рубль", FiatPM (FiatPaymentMethod "card2card") RUR)
+    , ("Перевод криптовалюты, PZM криптовалюта Prizm", CryptoPM PZM) ]
+
+typedCurrencyOptions :: [(CurrencyType, Currency)]
+typedCurrencyOptions =
+    [ (FiatT, FiatC RUR)
+    , (CryptoT, CryptoC PZM)
+    ]
+
+
+
+data Currency
+    = FiatC FiatCurrency
+    | CryptoC CryptoCurrency
+    deriving Eq
+
+data CurrencyType
+    = FiatT
+    | CryptoT
+    deriving Eq
+
+data FiatCurrency
+    = RUR
+    | USD
+    deriving Eq
+
+data CryptoCurrency
+    = BTC
+    | ETH
+    | PZM
+    deriving Eq
+
+data PaymentMethod
+    = FiatPM FiatPaymentMethod FiatCurrency
+    | CryptoPM CryptoCurrency
+    deriving Eq
+
+data FiatPaymentMethod = FiatPaymentMethod Text deriving Eq
