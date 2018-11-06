c0fa5718427d9539746aa3de59003688eabfae9d
Артур Файзрахманов <heraldhoi@gmail.com>
2018-11-06 09:08:55 +0500

    Add missing Fixed Fee calculation formula

--- src/Utils/Deposit.hs
+++ src/Utils/Deposit.hs
@@ -14,6 +14,7 @@ selectFee (CryptoC PZM) = depositFeePzm
 -- TODO: FIXME: User constant rather than numeral 100
 calcFeeCents :: Fee -> Int -> Int
 calcFeeCents (Percent p) c = ceiling $ fromIntegral c * p / 100
+calcFeeCents (CentsFixed f) c = c - f
 
 selectRatio' :: Currency -> Currency -> Double
 selectRatio' (CryptoC PZM) (FiatC RUR) = depositPzmRurRatio
