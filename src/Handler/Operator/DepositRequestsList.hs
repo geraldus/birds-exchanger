1608e6677798bb2a52fc72ba5a1bf430dbb03297
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-11-22 23:55:53 +0500

    Better deposit request list view for operators
    Fetch tuple of request, wallet and user from database using raw SQL.
    Better values rendering

--- src/Handler/Operator/DepositRequestsList.hs
+++ src/Handler/Operator/DepositRequestsList.hs
@@ -1,14 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Handler.Operator.DepositRequestsList where
 
 
-import Import
-import Local.Persist.Deposit
+import           Import
+import           Local.Persist.Deposit
+import           Utils.Deposit
 
-import Database.Persist.Sql (fromSqlKey)
+import           Database.Persist.Sql           ( fromSqlKey
+                                                , rawSql
+                                                )
+import           Data.Text.Format.Numbers       ( prettyF
+                                                , PrettyCfg(..)
+                                                )
 
 
 getOperatorDepositRequestsListR :: Handler Html
 getOperatorDepositRequestsListR = do
-    requireStaffId
-    depositRequestsList <- runDB $ selectList [DepositRequestStatus ==. ClientConfirmed] []
+    _    <- requireStaffId
+    list <-
+        runDB $ rawSql s [toPersistValue ClientConfirmed] :: Handler
+            [(Entity DepositRequest, Entity UserWallet, Entity User)]
+        -- selectList [DepositRequestStatus ==. ClientConfirmed] []
     defaultLayout $(widgetFile "operator/deposit-requests-list")
+  where
+    s
+        = "SELECT ??, ??, ?? FROM deposit_request, user_wallet, \"user\" \
+        \ WHERE deposit_request.status = ? \
+        \ AND deposit_request.archived = FALSE \
+        \ AND deposit_request.user_id = \"user\".id \
+        \ AND user_wallet.currency = deposit_request.currency \
+        \ AND user_wallet.user_id = deposit_request.user_id \
+        \ ORDER BY deposit_request.created ASC"
