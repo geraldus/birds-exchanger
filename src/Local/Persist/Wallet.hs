0fde6c2f3e7f9832881a3843cee3a92a8c81b90f
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-12-04 09:58:17 +0500

    Add docs

--- src/Local/Persist/Wallet.hs
+++ src/Local/Persist/Wallet.hs
@@ -9,13 +9,13 @@ import Database.Persist.TH
 data WalletTransactionType
     -- | Int's are money amount in cents.  All income operations
     --   must have positive sign; outcome -- negative.
-    = BalanceDeposit Int
-    | BalanceWithdrawal Int
-    | BalanceWithdrawalCancel Int
-    | ExchangeFreeze Int
-    | ExchangeReturn Int
-    | ExchangeExchange Int
-    | Penality Int
-    | Bonus Int
+    = BalanceDeposit Int          -- ^ + positive
+    | BalanceWithdrawal Int       -- ^ - negative
+    | BalanceWithdrawalCancel Int -- ^ + positive
+    | ExchangeFreeze Int          -- ^ - negative
+    | ExchangeReturn Int          -- ^ + positive
+    | ExchangeExchange Int        -- ^ + positive
+    | Penality Int                -- ^ - negative
+    | Bonus Int                   -- ^ + positive
     deriving (Show, Read, Eq)
 derivePersistField "WalletTransactionType"
