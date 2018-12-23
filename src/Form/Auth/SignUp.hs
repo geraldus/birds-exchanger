2ab3169cb5da738f786af9588ec07da1f5cb7b56
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-12-24 01:04:18 +0500

    Make sign up form follow bootstrap style

--- src/Form/Auth/SignUp.hs
+++ src/Form/Auth/SignUp.hs
@@ -7,7 +7,27 @@ import Type.Auth.SignUp
 
 
 signUpForm :: Html -> MForm Handler (FormResult SignUpFormData, Widget)
-signUpForm = renderDivs $ SignUpFormData
-    <$> areq textField "Эл.почта" Nothing
-    <*> areq passwordField "Пароль" Nothing
-    <*> areq passwordField "Подтверждение пароля" Nothing
+signUpForm extra = do
+    eid <- newIdent
+    pid <- newIdent
+    cid <- newIdent
+    (email, emailV) <- mreq emailField (fsAddPlaceholder (fsBs4WithId eid) "Ваш эл.ящик") Nothing
+    (passw, passwV) <- mreq passwordField (fsBs4WithId pid) Nothing
+    (confi, confiV) <- mreq passwordField (fsBs4WithId cid) Nothing
+    let widget = [whamlet|
+            #{extra}
+            <div .form-group.row>
+                <label for=#{eid}>Эл.почта
+                ^{fvInput emailV}
+            <div .form-group.row>
+                <label for=#{pid}>Пароль
+                ^{fvInput passwV}
+            <div .form-group.row>
+                <label for=#{cid}>Подтверждение пароля
+                ^{fvInput confiV}
+            |]
+    let res = SignUpFormData
+            <$> email
+            <*> passw
+            <*> confi
+    return (res, widget)
