cc45c59ea751f8ae4b024b596a63b8c44f31b0d8
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-12-28 17:17:10 +0500

    Do not redirect to referrer on login/logout

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -312,7 +312,7 @@ instance YesodAuth App where
     logoutDest _ = HomeR
     -- Override the above two destinations when a Referer: header is present
     redirectToReferer :: App -> Bool
-    redirectToReferer _ = True
+    redirectToReferer _ = False
 
     authenticate :: (MonadHandler m, HandlerSite m ~ App)
                  => Creds App -> m (AuthenticationResult App)
