b5abcea97e83b1d7ad18f00b438efa8a6337df06
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-12-28 17:15:36 +0500

    Update favicon

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -15,7 +15,7 @@ import Import
 getFaviconR :: Handler TypedContent
 getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
                  return $ TypedContent "image/x-icon"
-                        $ toContent $(embedFile "config/favicon.ico")
+                        $ toContent $(embedFile "config/favicon.png")
 
 getRobotsR :: Handler TypedContent
 getRobotsR = return $ TypedContent typePlain
