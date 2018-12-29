81f6c61f2f1f673725dee58633103648267f0aec
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2018-12-30 02:21:24 +0500

    Code clean up

--- src/Handler/Operator/AcceptDeposit.hs
+++ src/Handler/Operator/AcceptDeposit.hs
@@ -3,42 +3,56 @@
 module Handler.Operator.AcceptDeposit where
 
 
-import Import
-import Local.Persist.Deposit
-import Local.Persist.Wallet
-import Utils.Deposit
-import Type.Money (oneCoinCents)
+import           Import
+import           Local.Persist.Deposit
+import           Local.Persist.Wallet
+import           Utils.Deposit
+import           Type.Money                     ( oneCoinCents )
 
-import Database.Persist.Sql (toSqlKey)
+import           Database.Persist.Sql           ( toSqlKey )
 
 
 postOperatorAcceptDepositRequestR :: Handler Html
 postOperatorAcceptDepositRequestR = do
-    staffId <- requireStaffId
-    (drId, realAmount) <- (\(idt, amt) -> (toSqlKey idt, amt))
-        <$> runInputPost ((,)
-                <$> ireq intField "deposit-id"
-                <*> ireq doubleField "deposit-real-income")
+    staffId            <- requireStaffId
+    (drId, realAmount) <- (\(idt, amt) -> (toSqlKey idt, amt)) <$> runInputPost
+        ((,) <$> ireq intField "deposit-id" <*> ireq doubleField
+                                                     "deposit-real-income"
+        )
     mdepreq <- runDB $ get drId
     case mdepreq of
-        Nothing -> notFound
-        Just depreq@DepositRequest{..} -> do
+        Nothing                         -> notFound
+        Just DepositRequest {..} -> do
             mue <- runDB . get $ depositRequestUserId
             case mue of
                 Nothing -> notFound
-                Just u -> do
-                    walletEntity <- getOrCreateWallet depositRequestUserId depositRequestTargetCurrency
+                Just u  -> do
+                    walletEntity <- getOrCreateWallet
+                        depositRequestUserId
+                        depositRequestTargetCurrency
                     let (Entity userWalletId userWallet) = walletEntity
                     runDB $ do
                         -- TODO: FIXME: Save fee data
                         -- Save inner profit record
                         wtrId <- insert $ WalletTransactionReason userWalletId
-                        update drId [ DepositRequestStatus =. OperatorAccepted (pack . show $ staffId)]
-                        let realAmountCents = floor (realAmount * fromIntegral oneCoinCents)
-                            ratio = selectRatio' depositRequestCurrency depositRequestTargetCurrency
-                            fee = calcFeeCents (selectFee depositRequestCurrency) realAmountCents
+                        update
+                            drId
+                            [ DepositRequestStatus
+                                  =. OperatorAccepted (pack . show $ staffId)
+                            ]
+                        let realAmountCents =
+                                floor (realAmount * fromIntegral oneCoinCents)
+                            ratio = selectRatio'
+                                depositRequestCurrency
+                                depositRequestTargetCurrency
+                            fee = calcFeeCents
+                                (selectFee depositRequestCurrency)
+                                realAmountCents
                             realAmountToConvert = realAmountCents - fee
-                            targetAmountCents = truncate $ fromIntegral realAmountToConvert * ratio
+                            targetAmountCents =
+                                truncate
+                                    $ fromIntegral realAmountToConvert
+                                    * ratio
                         let realWalletIncome = targetAmountCents
                         let mStaffUserId = case staffId of
                                 Left uid -> Just uid
@@ -62,5 +76,6 @@ postOperatorAcceptDepositRequestR = do
                             wtrId
                             (userWalletAmountCents userWallet)
                             time
-                        update userWalletId [UserWalletAmountCents +=. realWalletIncome]
+                        update userWalletId
+                               [UserWalletAmountCents +=. realWalletIncome]
                     redirect OperatorDepositRequestsListR
