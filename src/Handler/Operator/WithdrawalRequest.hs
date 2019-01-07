233e03af074a673af300115c1942463d12589540
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-01-07 17:06:40 +0500

    Update withdrawal requests view.  Save fee data when accepted

--- src/Handler/Operator/WithdrawalRequest.hs
+++ src/Handler/Operator/WithdrawalRequest.hs
@@ -1,24 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Handler.Operator.WithdrawalRequest where
 
 
 import           Import
-import           Local.Persist.Deposit
-import           Local.Persist.Currency         ( currSign )
+import           Local.Persist.Currency
+import           Local.Persist.ExchangeOrder    ( ProfitType(..) )
+import           Utils.Render                   ( renderFeeAsPct )
+import           Utils.Time                     ( renderTimeDateRu )
+import           Utils.Withdrawal
 
+import qualified Data.Text                     as T
 import           Database.Persist.Sql           ( fromSqlKey
-                                                , toSqlKey
                                                 , rawSql
+                                                , toSqlKey
                                                 )
+import           Text.Blaze.Html                ( preEscapedToHtml )
 
 
 getOperatorWithdrawalRequestsListR :: Handler Html
 getOperatorWithdrawalRequestsListR = do
     requireStaffId
+    loc <- selectLocale
+    let reqDateT = renderTimeDateRu loc . withdrawalRequestCreated
     list <-
         runDB $ rawSql s [] :: Handler
             [(Entity WithdrawalRequest, Entity UserWallet, Entity User)]
-    defaultLayout $(widgetFile "operator/withdrawal-requests-list")
+    defaultLayout $ do
+        $(widgetFile "operator/request-list-common")
+        $(widgetFile "operator/withdrawal-requests-list")
   where
     s
         = "SELECT ??, ??, ?? FROM withdrawal_request, user_wallet, \"user\" \
@@ -34,9 +45,56 @@ postOperatorAcceptWithdrawalRequestR = do
     let mStaffUserId = case staffId of
             Left uid -> Just uid
             _        -> Nothing
-    wid  <- toSqlKey <$> runInputPost (ireq intField "withdrawal-id")
+    wid <- toSqlKey <$> runInputPost (ireq intField "withdrawal-id")
+    request <- runDB $ get404 wid
+    wallet <- runDB $ get404 (withdrawalRequestWalletId request)
+    fee <- runInputPost (ireq intField "withdrawal-fee")
+    trans <- runInputPost (ireq intField "withdrawal-transfered")
     time <- liftIO getCurrentTime
     runDB $ do
-        insert $ WithdrawalAccept wid (pack $ show staffId) mStaffUserId
+        insert $ WithdrawalAccept wid
+                                  trans
+                                  fee
+                                  (pack $ show staffId)
+                                  mStaffUserId
         update wid [WithdrawalRequestAccepted =. Just time]
+        insert $ InnerProfitRecord
+            (withdrawalRequestWalletTransactionReasonId request)
+            (userWalletCurrency wallet)
+            fee
+            WithdrawalFee
     redirect OperatorWithdrawalRequestsListR
+
+
+renderReqAddress :: Text -> Html
+renderReqAddress adr = preEscapedToHtml res
+  where
+    adrs = T.lines adr
+    res  = T.intercalate "<br>" adrs
+
+renderWalletCurrentFee :: UserWallet -> Html
+renderWalletCurrentFee w = renderFeeAsPct fee
+  where
+    currency = userWalletCurrency w
+    fee      = case currency of
+        (FiatC   RUR) -> defRurWithdrawalFee
+        (CryptoC PZM) -> defRurWithdrawalFee
+        c             -> error $ "no default fee for" <> show c
+
+renderReqFee :: UserWallet -> WithdrawalRequest -> Html
+renderReqFee w r = [shamlet|#{cents2dblT (reqFeeCents w r)}|]
+
+renderReqFeeCents :: UserWallet -> WithdrawalRequest -> Html
+renderReqFeeCents w r = [shamlet|#{show (reqFeeCents w r)}|]
+
+reqFeeCents :: UserWallet -> WithdrawalRequest -> Int
+reqFeeCents UserWallet {..} WithdrawalRequest {..} =
+    let fee = selectWithdrawalFee userWalletCurrency
+    in  calcFeeCents fee withdrawalRequestFrozenAmount
+
+reqTransferAmount :: UserWallet -> WithdrawalRequest -> Int
+reqTransferAmount w r =
+    let fee    = reqFeeCents w r
+        amount = withdrawalRequestFrozenAmount r - fee
+    in  amount
+
--- src/Utils/Withdrawal.hs
+++ src/Utils/Withdrawal.hs
@@ -1,10 +1,23 @@
 module Utils.Withdrawal
     ( defRurWithdrawalFee
     , defPzmWithdrawalFee
+    , selectWithdrawalFee
     )
 where
 
+import ClassyPrelude.Yesod
+
 import           Type.Fee                       ( Fee(..) )
+import           Local.Persist.Currency         ( Currency(..)
+                                                , FiatCurrency(..)
+                                                , CryptoCurrency(..)
+                                                )
+
+
+selectWithdrawalFee :: Currency -> Fee
+selectWithdrawalFee (FiatC   RUR) = defRurWithdrawalFee
+selectWithdrawalFee (CryptoC PZM) = defPzmWithdrawalFee
+selectWithdrawalFee c             = error $ "no default fee for " <> show c
 
 
 defRurWithdrawalFee :: Fee
