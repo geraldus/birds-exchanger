725dbad7399e51ec91f9dc2cd91890f71169451d
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-08 02:09:11 +0500

    Define SU socket connection address

--- src/Handler/SuperUser/FinancialReport.hs
+++ src/Handler/SuperUser/FinancialReport.hs
@@ -7,8 +7,16 @@ import           Import
 getSuperUserFinancialReportViewR :: Handler Html
 getSuperUserFinancialReportViewR = do
     su <- requireSu
+    renderUrl <- getUrlRender
     defaultLayout $ do
         addScriptRemote "https://unpkg.com/react@16/umd/react.development.js"
         addScriptRemote "https://unpkg.com/react-dom@16/umd/react-dom.development.js"
         addScriptAttrs (StaticR js_bundle_js) [("defer","defer")]
+        toWidget [julius|window.app = {
+            config: {
+                su: {
+                    socket: #{renderUrl SuperUserWebSocketR}.replace(/^http(s?:\/\/)/, 'ws$1')
+                }
+            }
+        }|]
         [whamlet|<div #react-host>|]
