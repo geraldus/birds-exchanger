12eaa0a27145d0f378040045fedf7df28f0c00f0
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-08 19:01:01 +0500

    Pass labels

--- react/src/main.js
+++ react/src/main.js
@@ -4,5 +4,11 @@ import { FinancialReportView } from "./staff/super-user/financial-report"
 $(document).ready(() => {
     const root = document.querySelector('#react-host')
     const wsAddr = window.app.config.su.socket
-    ReactDOM.render(<FinancialReportView socket={wsAddr}/>, root)
+    const lbls = window.app.config.su.labels
+    ReactDOM.render(
+        <FinancialReportView
+            socket={wsAddr}
+            labels={lbls}
+        />,
+        root)
 })
