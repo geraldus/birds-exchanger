ef53cd942a930da58734c40eb74de8b43a9e679d
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-08 18:56:25 +0500

    Configure report labels

--- src/Handler/SuperUser/FinancialReport.hs
+++ src/Handler/SuperUser/FinancialReport.hs
@@ -8,6 +8,7 @@ getSuperUserFinancialReportViewR :: Handler Html
 getSuperUserFinancialReportViewR = do
     su <- requireSu
     renderUrl <- getUrlRender
+    rm <- getMessageRender
     defaultLayout $ do
         addScriptRemote "https://unpkg.com/react@16/umd/react.development.js"
         addScriptRemote "https://unpkg.com/react-dom@16/umd/react-dom.development.js"
@@ -15,7 +16,20 @@ getSuperUserFinancialReportViewR = do
         toWidget [julius|window.app = {
             config: {
                 su: {
-                    socket: #{renderUrl SuperUserWebSocketR}.replace(/^http(s?:\/\/)/, 'ws$1')
+                    socket: #{renderUrl SuperUserWebSocketR}
+                        .replace(/^http(s?:\/\/)/, 'ws$1'),
+                    labels: {
+                        userCount: #{rm MsgClientUsersCount},
+                        innerProfit: #{rm MsgFeeStats},
+                        activeDeposits: #{rm MsgActiveDepositsCount},
+                        acceptedDeposits: #{rm MsgAcceptedDepositsCount},
+                        deposit: {
+                            income: {
+                                realTotal: #{rm MsgDepositIncomeRealTotal},
+                                feeTotal: #{rm MsgFee}
+                            }
+                        }
+                    }
                 }
             }
         }|]
