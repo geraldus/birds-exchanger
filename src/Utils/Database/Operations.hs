de7605dc454ffe16e57d96847c973f83bb919b4d
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-11 01:22:11 +0500

    Partially implement order save and execute database actions

--- src/Utils/Database/Operations.hs
+++ src/Utils/Database/Operations.hs
@@ -21,6 +21,100 @@ type FeeCents = Int
 
 type PositiveAmount = Int
 
+type LabeledError = (Text, Text)
+
+
+data OrderCheck
+    = OrderCheckErrors [ LabeledError ]
+    | OrderCheckSuccess (WalletTransactionReasonId -> ExchangeOrder)
+
+
+    -- | Transaction and its reason
+data TransactionD = TransactionD
+        (Entity WalletBalanceTransaction) (Entity WalletTransactionReason)
+
+-- | Details of income operation during order execution
+data ExecutionD = ExecutionD
+        (Entity ExchangeOrderExecution) TransactionD (Entity InnerProfitRecord)
+
+type OrderSaveData = (Entity ExchangeOrder, TransactionD)
+type OrderExecutionData = [ (ExecutionD, ExecutionD) ]
+
+data OrderInsertionDbData
+    = NoInsertion [ LabeledError ]
+    | Insertion OrderSaveData OrderExecutionData
+
+saveAndExecuteOrder
+    ::  ( MonadIO m
+        , PersistStoreWrite backend
+        , PersistQueryRead backend
+        , PersistUniqueRead backend
+        , BaseBackend backend ~ SqlBackend )
+    => UserId
+    -> AmountCents
+    -> Currency
+    -> UTCTime
+    -> (UserWallet -> OrderCheck)
+    -> ReaderT backend m OrderInsertionDbData
+saveAndExecuteOrder client a c t withWalletCheck = do
+    wallet <- getBy404 $ UniqueWallet client c
+    let orderCheck = withWalletCheck (entityVal wallet)
+    case orderCheck of
+        OrderCheckErrors es -> return $ NoInsertion es
+        OrderCheckSuccess withReasonOrder -> do
+            reason <- newWalletReason (entityKey wallet)
+            freeze <- decreaseUserWalletBalance
+                    wallet reason a ExchangeFreeze t
+            let orderSaveTransactionD = (freeze, reason)
+            let order = withReasonOrder reason
+            orderId <- insert order
+            let o = Entity orderId order
+            let orderSaveData = (o, orderSaveTransactionD)
+            executionData <- executeSavedOrder o wallet
+            error "123"
+
+executeSavedOrder
+    ::  ( MonadIO m
+        , PersistStoreWrite backend
+        , PersistQueryRead backend
+        , BaseBackend backend ~ SqlBackend )
+    => Entity ExchangeOrder
+    -> Entity UserWallet
+    -> ReaderT backend m OrderExecutionData
+executeSavedOrder o w = do
+    ms <- findMatchingOrders o
+    error "!23"
+
+executeExchange
+    ::  ( MonadIO m
+    , PersistStoreWrite backend
+    , PersistQueryRead backend
+    , BaseBackend backend ~ SqlBackend )
+    => Entity ExchangeOrder
+    -> Entity ExchangeOrder
+    -> ( Entity ExchangeOrder, [ OrderExecutionData ] )
+    -> ReaderT backend m ( Entity ExchangeOrder, [ OrderExecutionData ] )
+executeExchange target match acc = do
+    let (Entity _ t) = target
+    let (Entity _ m) = match
+    let (tLeft, tPair, tK, tRatio, tExpected) = orderParams t
+    let (mLeft, mPair, mK, mRatio, mExpected) = orderParams m
+    when (mPair /= flipPair tPair) $
+        error "Impossible happened!  Order pairs not match"
+    when (wrongRatio tPair tK mK) $
+        error "Impossible happened!  Wrong ratio condition"
+    error "execution step"
+  where
+    orderParams ExchangeOrder{..} =
+        let l = exchangeOrderAmountLeft
+            p = exchangeOrderPair
+            k = exchangeOrderNormalizedRatio
+            r = pairRatioByNormalizedRatio p k
+            e = multiplyCents r l
+        in (l, p, k, r, e)
+    wrongRatio tp tk mk =
+        if defPairDir tp == tp then mk > tk else mk < tk
+
 findMatchingOrders
     ::  ( MonadIO m
         , PersistStoreWrite backend
