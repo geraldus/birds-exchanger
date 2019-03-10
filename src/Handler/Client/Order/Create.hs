6205c92dd31ab868e77841c5f741f0707bb0de75
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-11 01:23:02 +0500

    Partially rewrite order save and execution handlers

--- src/Handler/Client/Order/Create.hs
+++ src/Handler/Client/Order/Create.hs
@@ -12,9 +12,126 @@ import           Form.Exchanger.Order
 import           Local.Persist.Currency
 import           Local.Persist.Exchange
 import           Local.Persist.Wallet
+import           Utils.Database.Operations
 import           Utils.Money
 
-import           Data.Maybe             ( fromJust )
+import           Control.Monad             ( void )
+import           Data.Maybe                ( fromJust )
+
+
+
+data ProcessForm
+    = ProcessFormNoData
+    | ProcessFormErrors [ LabeledError ]
+    | ProcessFormSuccess
+            AmountCents Currency UTCTime (UserWallet -> OrderCheck)
+
+
+processNewOrder
+    :: UserId
+    -> ExchangePair
+    -> AmountCents
+    -> NormalizedRatio
+    -> UTCTime
+    -> (AppMessage -> Text)
+    -> ProcessForm
+processNewOrder client epair a r t mr =
+    let er a' r' w' = orderCreateRenderFormErrors a' r' w' mr
+        check w = checkOrderData epair a r client w t er
+        c = fst . unPairCurrency $ epair
+    in ProcessFormSuccess a c t check
+
+checkOrderData
+    :: ExchangePair
+    -> AmountCents
+    -> NormalizedRatio
+    -> UserId
+    -> UserWallet
+    -> UTCTime
+    -> (Int -> Double -> UserWallet -> [ LabeledError ])
+    -> OrderCheck
+checkOrderData
+        exchangeDirection
+        amount
+        ratio
+        client
+        wallet
+        time
+        renderErrors =
+    if amount <= 0 || ratio <= 0 || amount > userWalletAmountCents wallet
+        then OrderCheckErrors (renderErrors amount ratio wallet)
+        else
+            let r = pairRatioByNormalizedRatio exchangeDirection ratio
+                -- Ratio Multiplicator
+                -- Ratio within form data is in normalized state, convert it
+                -- to direct multiplication value
+                t = multiplyCents r amount
+                -- Target Amount
+                -- Amount cents required for full order exchange
+                f = calcFeeCents defaultExchangeFee t
+                -- Expected fee, shown for user when order was created
+                -- d = giveTake action pair (flipPair epair)
+                d = exchangeDirection
+                -- Correct Exchange Direction
+                -- Pair within form data doesn't represent correct exchange
+                -- direction, it must be interpreted depending on 'action', e.g.
+                -- give action -- same direction, take action -- flipped.
+            in OrderCheckSuccess $
+                    mkNewOrderData client amount ratio d t f time
+
+orderCreateRenderFormErrors
+    :: AmountCents
+    -> NormalizedRatio
+    -> UserWallet
+    -> (AppMessage -> Text)
+    -> [ LabeledError ]
+orderCreateRenderFormErrors a r w m =
+    let amount = mkError (a <= 0) "amount" (m MsgPositiveValueRequired)
+        ratio  = mkError (r <= 0) "ratio" (m MsgPositiveValueRequired)
+        balance = mkError
+                (userWalletAmountCents w < a) "balance" (m MsgNotEnoughFunds)
+    in amount <> ratio <> balance
+  where mkError cond name e = [ (name, e) | cond ]
+
+
+giveTake :: ExchangeAction -> p -> p -> p
+giveTake a giveChoise takeChoise = if a == EAGive
+    then giveChoise else takeChoise
+
+
+postExchangeOrderCreateR' :: ExchangePair -> Handler Html
+postExchangeOrderCreateR' epair = do
+    client <- requireClientId
+    rid <- newIdent
+    wid <- newIdent
+    ((res, _), _) <- runFormPost $ createOrderForm wid rid epair
+    proceessResult <- case res of
+        FormMissing    -> return ProcessFormNoData
+        FormFailure es -> return $
+                ProcessFormErrors $ zip es (repeat "form")
+        FormSuccess od -> do
+            let a = amount od
+                r = ratio od
+                act = action od
+                formPair = pair od
+                d = giveTake act formPair (flipPair formPair)
+                outCurrency = fst . unPairCurrency $ d
+            mr <- getMessageRender
+            t <- liftIO getCurrentTime
+            return $ processNewOrder client d a r t mr
+    case proceessResult of
+        ProcessFormErrors es -> do
+            -- TODO: FIXME: Add JSON response capabilities
+            let msg = [shamlet|$forall (_, message) <- es
+                    <div>#{message} |]
+            setMessage msg
+        ProcessFormSuccess a c t checkOrder -> do
+            runDB $ saveAndExecuteOrder client a c t checkOrder
+            return ()
+        _ -> return ()
+    redirect HomeR
+
+
 
 
 postExchangeOrderCreateR :: Handler Html
