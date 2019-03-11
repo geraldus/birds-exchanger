5072242a5036da0cebed6c0c1ca489bd1923e01a
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-12 00:10:02 +0500

    Implement all micro steps for order execution

--- src/Utils/Database/Operations.hs
+++ src/Utils/Database/Operations.hs
@@ -73,15 +73,107 @@ executeExchange target match acc = do
         error "Impossible happened!  Wrong ratio condition"
     error "execution step"
   where
-    orderParams ExchangeOrder{..} =
-        let l = exchangeOrderAmountLeft
-            p = exchangeOrderPair
-            k = exchangeOrderNormalizedRatio
-            r = pairRatioByNormalizedRatio p k
-            e = multiplyCents r l
-        in (l, p, k, r, e)
-    wrongRatio tp tk mk =
-        if defPairDir tp == tp then mk > tk else mk < tk
+
+    execute = do
+        let (Entity tid t) = target
+        let (Entity mid m) = match
+        let mPair = exchangeOrderPair m
+        let tclient = exchangeOrderUserId t
+        let mclient = exchangeOrderUserId m
+        let (tCurrencyIn, mCurrencyIn) = unPairCurrency mPair
+        let p = exchangeParams t m
+        ws@(tWallet, mWallet) <- getWallets404
+                (tclient, tCurrencyIn) (mclient, mCurrencyIn)
+        (tReason, mReason) <- mkReasons ws
+        let tWalletReason = (tWallet, tReason)
+        let mWalletReason = (mWallet, mReason)
+        let tS = exchangeOrderStatus t
+        let mS = exchangeOrderStatus m
+        ((tStatus, tExecution, tP), (_, mExecution, mP), diffProfit) <-
+                mkExchange (tid, tS) (mid, mS) tReason mReason p
+        (tProfit, mProfit) <- saveFee
+                (tReason, tCurrencyIn, tP) (mReason, mCurrencyIn, mP)
+        let (tIn, tOut, tFee, tClosed) = tP
+        let (mIn, mOut, mFee, _) = mP
+        let tInc = (tWalletReason, tIn - tFee)
+        let mInc = (mWalletReason, mIn - mFee)
+        (tBalance, mBalance) <- increaseBalances tInc mInc
+        mayDiffProfit <- saveDiffProfit mReason mCurrencyIn diffProfit
+        let tData = ExecutionD
+                tExecution (TransactionD tBalance tReason) tProfit mayDiffProfit
+        let mData = ExecutionD
+                mExecution (TransactionD mBalance mReason) mProfit Nothing
+        let tLeft' = exchangeOrderAmountLeft t
+        let tUpdated = t
+                { exchangeOrderStatus = tStatus
+                , exchangeOrderAmountLeft = if tClosed then 0 else tLeft' - tOut
+                , exchangeOrderIsActive = tClosed
+                }
+        return (tClosed, (tData, mData), tUpdated)
+
+    getWallets404 (c1, cur1) (c2, cur2)= do
+        w1 <- getBy404 $ UniqueWallet c1 cur1
+        w2 <- getBy404 $ UniqueWallet c2 cur2
+        return (w1, w2)
+
+    mkReasons (w1, w2) = (,)
+        <$> newWalletReason (entityKey w1)
+        <*> newWalletReason (entityKey w2)
+
+    mkExchange (o1, s1) (o2, s2) r1 r2 params = do
+        let (p1, p2, dp) = params
+        let (in1, out1, fee1, closed1) = p1
+            (in2, out2, fee2, closed2) = p2
+        let (u1, us1) = updates closed1 out1 s1
+            (u2, us2) = updates closed2 out2 s2
+        let e1 = ExchangeOrderExecution o1 time r2 r1 closed1 out1 in1 fee1
+            e2 = ExchangeOrderExecution o2 time r1 r2 closed2 out2 in2 fee2
+        update o1 u1
+        update o2 u2
+        e1id <- insert e1
+        e2id <- insert e2
+        return
+            ((us1, Entity e1id e1, p1), (us2, Entity e2id e1, p2), dp)
+      where
+        updates closed x s'
+            | closed =
+                let s = Executed time
+                    u = [ ExchangeOrderStatus =. s
+                        , ExchangeOrderAmountLeft =. 0
+                        , ExchangeOrderIsActive =. not closed ]
+                in (u, s)
+            | otherwise =
+                let s = partial s' x
+                    u = [ ExchangeOrderStatus =. s
+                        , ExchangeOrderAmountLeft -=. x
+                        , ExchangeOrderIsActive =. not closed ]
+                in (u, s)
+        partial (PartiallyExecuted _ p) x =  PartiallyExecuted time (p + x)
+        partial _ x                       = PartiallyExecuted time x
+
+    saveFee (r1, c1, p1) (r2, c2, p2) = do
+        let (_, _, f1, _) = p1
+        let (_, _, f2, _) = p2
+        let i1 = InnerProfitRecord r1 c1 f1 ExchangeFee
+        let i2 = InnerProfitRecord r2 c2 f2 ExchangeFee
+        i1id <- insert i1
+        i2id <- insert i2
+        return (Entity i1id i1, Entity i2id i2)
+
+    increaseBalances
+        ((w1, r1), in1) ((w2, r2), in2) = do
+            t1 <- addUserWalletBalance w1 r1 in1 ExchangeExchange time
+            t2 <- addUserWalletBalance w2 r2 in2 ExchangeExchange time
+            return (t1, t2)
+
+    saveDiffProfit r c dp
+        | dp < 0 = error "Impossible happened: Diff profit is negative"
+        | dp > 0 = do
+            let tr = InnerProfitRecord r c dp ExchangeDiff
+            insert tr >>= \x -> pure . Just . Entity x  $ tr
+        | otherwise = pure Nothing
+
+    wrongRatioCase tk mk = dirPairMatch (mk > tk) (mk < tk)
 
 findMatchingOrders
     ::  ( MonadIO m
