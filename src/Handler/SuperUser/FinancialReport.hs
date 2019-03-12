9fe4ca1953858cce2e064223f4c76d28f47bc1c1
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-12 23:44:13 +0500

    Move JS code to template file

--- src/Handler/SuperUser/FinancialReport.hs
+++ src/Handler/SuperUser/FinancialReport.hs
@@ -21,41 +21,5 @@ getSuperUserFinancialReportViewR = do
         addScriptRemote $ "https://unpkg.com/react@16/umd/react." <> reactBuild <> ".js"
         addScriptRemote $ "https://unpkg.com/react-dom@16/umd/react-dom." <> reactBuild <> ".js"
         addScriptAttrs (StaticR js_bundle_js) []
-        toWidget [julius|window.app = {
-            config: {
-                su: {
-                    socket: #{renderUrl SuperUserWebSocketR}
-                        .replace(/^http(s?:\/\/)/, 'ws$1'),
-                    labels: {
-                        userCount: #{rm MsgClientUsersCount},
-                        innerProfit: #{rm MsgFeeStats},
-                        activeDeposits: #{rm MsgActiveDepositsCount},
-                        acceptedDeposits: #{rm MsgAcceptedDepositsCount},
-                        deposit: {
-                            stats: #{rm MsgDepositStatsTitle},
-                            income: {
-                                realTotal: #{rm MsgDepositIncomeRealTotal},
-                                feeTotal: #{rm MsgFee}
-                            }
-                        },
-                        wallets: {
-                            balanceTotals: #{rm MsgUserWalletBalancesTotal}
-                        },
-                        withdrawal: {
-                            stats: #{rm MsgWithdrawalStatsTitle},
-                            new: {
-                                count: #{rm MsgWithdrawalAwaitingExecution},
-                                amount: #{rm MsgWithdrawalAmountTotal},
-                                frozen: #{rm MsgWithdrawalFrozenTotal}
-                            },
-                            accepted: {
-                                count: #{rm MsgWithdrawalAcceptedCount},
-                                transfered: #{rm MsgWithdrawalTransferedTotal},
-                                fee: #{rm MsgWithdrawalFeeTotal}
-                            }
-                        }
-                    }
-                }
-            }
-        }|]
-        [whamlet|<div #react-host>|]
+        rootId <- newIdent
+        $(widgetFile "su/financial-report")
