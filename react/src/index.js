f1dfc4ba3965d79d6cf84b29730a72ed459c2c3c
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-03-24 13:01:26 +0500

    Implement view and deposit handler for MAO Accounting Table

--- react/src/index.js
+++ react/src/index.js
@@ -1,5 +1,7 @@
-import FinancialReportView from "./staff/super-user/financial-report"
-import Notifier from "./staff/operator/notifier"
+import FinancialReportView from './staff/super-user/financial-report'
+import Notifier from './staff/operator/notifier'
+import MaoOperatorAccountingTable from './staff/mao/operator/accounting-table.js'
 
 export const financialReportView = FinancialReportView
 export const notifier = Notifier
+export const maoOperatorAccountingTable = MaoOperatorAccountingTable
