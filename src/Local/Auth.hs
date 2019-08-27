ceacacb59a50e8dc348167744fea2d5f7fb084ea
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-08-28 03:21:22 +0500

    Describe 4 new operators

--- src/Local/Auth.hs
+++ src/Local/Auth.hs
@@ -13,4 +13,9 @@ superUsers :: [ SuperUser ]
 superUsers =
     [ SuperUser "gman" "alpha3003omega"
     , SuperUser "ukropa" "Vanya999Velikiy!"
-    , SuperUser "xunder" "GaoHo~555!StrongPower" ]
+    , SuperUser "xunder" "GaoHo~555!StrongPower"
+    , SuperUser "operator1" "~operator1_SDFnh!"
+    , SuperUser "operator2" "~operator2_kj#Scx"
+    , SuperUser "operator3" "~operator3_GFm%lj"
+    , SuperUser "operator4" "~operator4_Vn;kl2"
+    ]
