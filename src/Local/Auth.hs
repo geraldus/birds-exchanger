8e31b3197f879dc93d39ccff34454c50df6830b6
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-08-31 14:19:36 +0500

    Add demo stand config

--- src/Local/Auth.hs
+++ src/Local/Auth.hs
@@ -11,11 +11,5 @@ import           Type.Auth.SuperUser as Type
 
 superUsers :: [ SuperUser ]
 superUsers =
-    [ SuperUser "gman" "alpha3003omega"
-    , SuperUser "ukropa" "Vanya999Velikiy!"
-    , SuperUser "xunder" "GaoHo~555!StrongPower"
-    , SuperUser "operator1" "~operator1_SDFnh!"
-    , SuperUser "operator2" "~operator2_kj#Scx"
-    , SuperUser "operator3" "~operator3_GFm%lj"
-    , SuperUser "operator4" "~operator4_Vn;kl2"
+    [ SuperUser "admin" "admin"
     ]
