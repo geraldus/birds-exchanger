60d5cdfedc136affcee8da09abecc5647fa75397
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-09-05 20:21:06 +0500

    Update emacs local vars

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,2 @@
-((haskell-mode . ((haskell-indent-spaces . 4)
-                  (haskell-process-use-ghci . t)))
- (hamlet-mode . ((hamlet/basic-offset . 4)
-                 (haskell-process-use-ghci . t))))
+((haskell-mode . ((haskell-indent-spaces . 4)))
+ (hamlet-mode . ((hamlet/basic-offset . 4))))
