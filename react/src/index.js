7169fa23905011b377f1db8bd9d64b97ad2cab2b
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-10-11 23:09:45 +0500

    Move UI sources out of repository

--- react/src/index.js
+++
@@ -1,7 +0,0 @@
-import FinancialReportView from './staff/super-user/financial-report'
-import Notifier from './staff/operator/notifier'
-import MaoOperatorAccountingTable from './staff/mao/operator/accounting-table.js'
-
-export const financialReportView = FinancialReportView
-export const notifier = Notifier
-export const maoOperatorAccountingTable = MaoOperatorAccountingTable
