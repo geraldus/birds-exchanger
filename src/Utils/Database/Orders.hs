c55ada85ff161ad04e7201d92921140de9b24597
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-10-12 03:38:00 +0500

    Implement active orders generic selector

---
+++ src/Utils/Database/Orders.hs
@@ -0,0 +1,25 @@
+module Utils.Database.Orders where
+
+import           Import.NoFoundation    as I hiding ( (==.), (||.) )
+import           Local.Persist.Exchange
+import           Type.Market
+import           Utils.Money
+
+import           Database.Esqueleto     as E
+
+
+selectActiveOrdersOf
+    :: (MonadIO m)
+    => ExchangePair
+    -> SqlPersistT m [OrderUserP]
+selectActiveOrdersOf p = select . from $ \(o, u) -> do
+    where_ (
+            (o ^. ExchangeOrderIsActive ==. val True)
+        &&. (o ^. ExchangeOrderUserId   ==. u ^. UserId)
+        &&. (
+                (o ^. ExchangeOrderPair ==. val p)
+            ||. (o ^. ExchangeOrderPair ==. val (flipPair p))))
+    orderBy
+        [ asc (o ^. ExchangeOrderNormalizedRatio)
+        , asc (o ^. ExchangeOrderCreated) ]
+    return (o, u)
