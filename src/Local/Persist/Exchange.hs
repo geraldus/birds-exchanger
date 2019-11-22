33bb64fd19accd30157b64c0d298f81d8fb4aa8b
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-11-22 16:27:03 +0500

    Add safe and unsafe exchange pair generator

--- src/Local/Persist/Exchange.hs
+++ src/Local/Persist/Exchange.hs
@@ -2,10 +2,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Local.Persist.Exchange where
 
-import           ClassyPrelude.Yesod
+import           Local.Persist.Currency ( CryptoCurrency (..), Currency (..),
+                                          FiatCurrency (..) )
 
+import           ClassyPrelude.Yesod
 import           Data.Aeson
-import           Data.Time.Clock     ( UTCTime )
+import           Data.Time.Clock        ( UTCTime )
 
 
 data ExchangeOrderStatus
@@ -14,9 +16,13 @@ data ExchangeOrderStatus
     | PartiallyExecuted UTCTime Int  -- ^ last mod time and executed cents total
     | Executed UTCTime
     deriving (Show, Read, Eq, Generic)
+
 instance FromJSON ExchangeOrderStatus
+
 instance ToJSON ExchangeOrderStatus
+
 instance ToJSONKey ExchangeOrderStatus
+
 derivePersistField "ExchangeOrderStatus"
 
 data ProfitType
@@ -25,8 +31,11 @@ data ProfitType
     | ExchangeFee
     | ExchangeDiff
     deriving (Show, Read, Eq, Generic)
+
 instance ToJSON ProfitType
+
 instance FromJSON ProfitType
+
 derivePersistField "ProfitType"
 
 
@@ -49,18 +58,51 @@ data ExchangePair
     | ExchangeRurOur
     | ExchangeOurRur
     deriving (Show, Read, Eq, Generic)
+
 instance ToJSON ExchangePair where
     toJSON = String . pack . show
+
 instance FromJSON ExchangePair where
     parseJSON = withText "ExchangePair Haskell Type Value" $ \v -> return $
         case readMay v of
             Just p  -> p
             Nothing -> error "Haskell Value read error"
+
 instance ToJSONKey ExchangePair
+
 instance Hashable ExchangePair where
     hash = hash . show
+
 derivePersistField "ExchangePair"
 
 instance PathPiece ExchangePair where
     toPathPiece = pack . show
     fromPathPiece = readMay
+
+exchangePairUnsafe :: Currency -> Currency -> ExchangePair
+-- | RUB
+exchangePairUnsafe (FiatC RUR)   (CryptoC PZM) = ExchangeRurPzm
+exchangePairUnsafe (FiatC RUR)   (CryptoC OUR) = ExchangeRurOur
+-- | PZM
+exchangePairUnsafe (CryptoC PZM) (FiatC RUR)   = ExchangePzmRur
+exchangePairUnsafe (CryptoC PZM) (CryptoC OUR) = ExchangePzmOur
+-- | OURO
+exchangePairUnsafe (CryptoC OUR) (FiatC RUR)   = ExchangeOurRur
+exchangePairUnsafe (CryptoC OUR) (CryptoC PZM) = ExchangeOurPzm
+-- | Other
+exchangePairUnsafe c1 c2 = error $
+    "Unexpected exchange pair: " <> show c1 <> ", "  <> show c2
+
+
+exchangePair :: Currency -> Currency -> Maybe ExchangePair
+-- RUB
+exchangePair (FiatC RUR)   (CryptoC PZM) = Just ExchangeRurPzm
+exchangePair (FiatC RUR)   (CryptoC OUR) = Just ExchangeRurOur
+-- PZM
+exchangePair (CryptoC PZM) (FiatC RUR)   = Just ExchangePzmRur
+exchangePair (CryptoC PZM) (CryptoC OUR) = Just ExchangePzmOur
+-- OURO
+exchangePair (CryptoC OUR) (FiatC RUR)   = Just ExchangeOurRur
+exchangePair (CryptoC OUR) (CryptoC PZM) = Just ExchangeOurPzm
+-- Other
+exchangePair _ _ = Nothing
