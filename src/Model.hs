a2c3a7bb39a3d6aa737e45274c79cc44ef05a224
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2019-12-09 18:04:03 +0500

    Maually define JSON instance for User model to prevent password leaks

--- src/Model.hs
+++ src/Model.hs
@@ -10,12 +10,16 @@
 module Model where
 
 import           ClassyPrelude.Yesod
-import           Database.Persist.Quasi
 
 import           Local.Persist.Currency
 import           Local.Persist.Exchange
+import           Local.Persist.Notice
 import           Local.Persist.UserRole
 import           Local.Persist.Wallet
+
+import           Database.Persist.Quasi
+import           Database.Persist.Sql   ( fromSqlKey )
+
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
@@ -23,3 +27,9 @@ import           Local.Persist.Wallet
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+instance ToJSON (Entity User) where
+    toJSON (Entity uid (User ident _ role)) = object
+        [ "id" .= fromSqlKey uid
+        , "ident" .= toJSON ident
+        , "role" .= toJSON role ]
