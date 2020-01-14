45875edb2fefe6db59025d6f82356022af7c5a45
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2020-01-15 04:07:24 +0500

    Get rid of super operators

--- src/Local/Auth.hs
+++ src/Local/Auth.hs
@@ -11,11 +11,7 @@ import           Type.Auth.SuperUser as Type
 
 superUsers :: [ SuperUser ]
 superUsers =
-    [ SuperUser "gman" "alpha3003omega"
+    [ SuperUser "gman"   "alpha3003omega"
     , SuperUser "ukropa" "Vanya999Velikiy!"
     , SuperUser "xunder" "GaoHo~555!StrongPower"
-    , SuperUser "operator1" "~operator1_SDFnh!"
-    , SuperUser "operator2" "~operator2_kj#Scx"
-    , SuperUser "operator3" "~operator3_GFm%lj"
-    , SuperUser "operator4" "~operator4_Vn;kl2"
     ]
