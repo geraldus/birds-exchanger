6cee36bf1589f5c279e22238c285f7340be97abd
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2020-01-15 04:16:31 +0500

    Notify all operators about confirmed purchases via email

--- src/Handler/Client/Stocks/Purchase/Confirmation.hs
+++ src/Handler/Client/Stocks/Purchase/Confirmation.hs
@@ -1,9 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Handler.Client.Stocks.Purchase.Confirmation where
 
-import           Import
+import           Import                                   hiding ( on, update,
+                                                            (==.) )
 
-import           Utils.Database.User.Stocks ( queryClientPurchasesByToken )
+import           Handler.Operator.Stocks.Purchase.Details ( queryPurchaseFullDetails )
+import           Local.Persist.Notice                     ( NoticeType (NoticeEmail) )
+import           Local.Persist.UserRole                   ( UserRole (Operator) )
+import           Utils.App.Common                         ( sendNoReplyEmail )
+import           Utils.Common                             ( getFormatDateRender,
+                                                            getFormatTimeRender )
+import           Utils.Database.User.Stocks               ( queryClientPurchasesByToken )
+import           Utils.Money                              ( cents2dblT )
+import           Utils.QQ                                 ( stFile )
+import           Utils.Stocks                             ( purchaseSignificantDate )
+
+import           Data.Aeson                               ( encode )
+import qualified Data.Text.Lazy                           as TL
+import           Database.Esqueleto
+import qualified Database.Persist                         as P
+import           Text.Blaze.Html.Renderer.Text            ( renderHtml )
+import           Text.Hamlet                              ( shamletFile )
 
 
 postClientStocksPurchaseConfirmationR :: Text -> Handler TypedContent
@@ -38,8 +55,101 @@ postClientStocksPurchaseConfirmationR token = do
                 respondJSONErrorMessages (render message)    [ ]
             (Entity pid _, _) : _ -> do
                 timeNow <- liftIO getCurrentTime
-                runDB $ update
+                runDB $ P.update
                     pid
-                    [ StocksPurchaseUserConfirmed =. Just timeNow
-                    , StocksPurchasePayerAddress =. Just payerWallet ]
+                    [ StocksPurchaseUserConfirmed P.=. Just timeNow
+                    , StocksPurchasePayerAddress  P.=. Just payerWallet ]
+                notifyOperatorStocksPurchase pid
                 redirect (ClientStocksPurchaseDetailsR token)
+
+notifyOperatorStocksPurchase :: StocksPurchaseId -> Handler ()
+notifyOperatorStocksPurchase pid = do
+    vals <- runDB . queryPurchaseFullDetails $ pid
+    case vals of
+        [] -> do
+            addMessage "invalid-url" "Shouln't happen. No purchase found"
+            redirect OperatorStocksPurchaseIndexR
+        (p, Entity _ s, Entity _ a, Entity _ u, Entity _ e) : _ -> do
+            typ           <- appType . appSettings <$> getYesod
+            urlRender     <- getUrlRender
+            messageRender <- getMessageRender
+            renderDate    <- getFormatDateRender
+            renderTime    <- getFormatTimeRender
+            tn            <- liftIO getCurrentTime
+            operators     <- ("heraldhoi@gmail.com" :) <$> operatorEmailsFromDB
+            let packName = stocksName s
+            let url      = urlRender (OperatorStocksPurchaseDetailsR (entityKey p))
+                sigDate  = purchaseSignificantDate $ entityVal p
+                date     = renderDate sigDate <> " " <> renderTime sigDate
+            let txt      = textContent (s, a) p u e url date
+                html     = htmlContent (s, a) p u e url date
+            let contents = toStrict . decodeUtf8 . encode $ object
+                    [ "text-content" .= txt
+                    , "html-content" .= html ]
+                subject = messageRender $
+                        MsgEmailSubjectOperatorPendingPurchase packName
+            flip mapM_ operators $ \email -> do
+                let n = Notice
+                        NoticeEmail email contents tn Nothing 0 Nothing (Just tn)
+                _ <- runDB $ insert n
+                void . liftIO $
+                    sendNoReplyEmail typ messageRender email subject txt html
+                -- ^ TODO update notice status
+
+operatorEmailsFromDB :: Handler [Text]
+operatorEmailsFromDB = do
+    vals <- runDB queryActiveOperators
+    return $ map (emailEmail . entityVal . snd) vals
+
+queryActiveOperators :: MonadIO m => SqlPersistT m [(Entity User, Entity Email)]
+queryActiveOperators = select . from  $
+    \(u `InnerJoin` e) -> do
+        on (just (u ^. UserId) ==. e ^. EmailUserId)
+        where_ (u ^. UserRole ==. val Operator)
+        return (u, e)
+
+textContent ::
+       (Stocks, StocksActive)
+    -> Entity StocksPurchase
+    -> User
+    -> Email
+    -> Text
+    -> Text
+    -> TL.Text
+textContent (s, a) p u e url date = let
+    (stocksPack, stocksAmount, cost, transferAddr, payer, username, stocksLeft)
+        = renderValues (s, a) p u e
+    in fromStrict $
+        [stFile|templates/mail/operator/stocks/purchase-confirmed.text|]
+
+htmlContent ::
+       (Stocks, StocksActive)
+    -> Entity StocksPurchase
+    -> User
+    -> Email
+    -> Text
+    -> Text
+    -> TL.Text
+htmlContent (s, a) p u e url date = let
+    (stocksPack, stocksAmount, cost, transferAddr, payer, username, stocksLeft)
+        = renderValues (s, a) p u e
+    in renderHtml $(shamletFile "templates/mail/operator/stocks/purchase-confirmed.hamlet")
+
+
+renderValues ::
+       (Stocks, StocksActive)
+    -> Entity StocksPurchase
+    -> User
+    -> Email
+    -> (Text, String, Text, Text, Text, Text, String)
+renderValues (s, a) (Entity _ p) _ e = let
+    amount       = stocksPurchaseAmount p
+    price        = stocksPrice s
+    stocksPack   = stocksName s
+    stocksAmount = show amount
+    cost         = cents2dblT (amount * price)
+    transferAddr = stocksPurchaseTransferAddress p
+    payer        = fromMaybe "не указан" (stocksPurchasePayerAddress p)
+    username     = emailEmail e
+    stocksLeft   = show (stocksActiveLeft a)
+    in (stocksPack, stocksAmount, cost, transferAddr, payer, username, stocksLeft)
