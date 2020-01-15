4041c23b3c83a2499a75fccbab02aa69a5eddef0
Arthur S. Fayzrakhmanov <heraldhoi@gmail.com>
2020-01-16 00:02:03 +0500

    Fix bug: prevent dummy empty value override actual ZLT balance rendered

--- src/Handler/Client/Stocks/Purchase/Details.hs
+++ src/Handler/Client/Stocks/Purchase/Details.hs
@@ -10,7 +10,7 @@ import           Utils.App.Client           ( dateTimeRowW )
 import           Utils.Database.User.Stocks ( queryClientPurchasesByToken )
 import           Utils.Stocks               ( purchaseSignificantDate )
 
-import           Data.Aeson                 ( decode )
+import           Data.Aeson                 ( decode, encode )
 import           Text.Julius                ( rawJS )
 
 
@@ -20,6 +20,7 @@ getClientStocksPurchaseDetailsR token = do
     let isClientLoggedIn = True
     htmlId <- newIdent
     urlRender <- getUrlRender
+    (_, stocks) <- getUserBalances
     withExistingClientPurchase client $ \(Entity _ p, Entity _ s) -> do
         let abr             = stocksAbbr s
         let purchaseDate    = (dateTimeRowW . purchaseSignificantDate) p
@@ -31,7 +32,8 @@ getClientStocksPurchaseDetailsR token = do
         let guide           = paymentGuideW p s
         let pageUrl         = urlRender (ClientStocksPurchaseDetailsR token)
         let clientSocketUrl = urlRender ClientNotificationsWebSocketR
-        let jsonStocksPurchases = "[]" :: Text
+        let jsonStocksPurchases = decodeUtf8 . encode $
+                map clientStocksToJSON stocks
         selectRep . provideRep . defaultLayout $ do
             setAppTitle [ MsgPageTitleStocksDetailsAbbr abr ]
             $(widgetFile "client/stocks/purchases/details")
